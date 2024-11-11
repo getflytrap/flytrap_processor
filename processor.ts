637b5452f1365cde7526ed80d640e733d772f1e7
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-11 13:58:57 +0100

    fix: remove response sending mechanism since API gateway will handle it
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>

--- processor.ts
+++ processor.ts
@@ -1,6 +1,5 @@
-import { saveErrorData, savePromiseData } from "./helpers/db.js";
-import { sendWebhookNotification } from "./helpers/webhook.js";
-import { createResponse } from "./helpers/response.js";
+import { saveErrorData, saveRejectionData } from "./helpers/database";
+import { sendWebhookNotification } from "./helpers/webhook";
 
 interface EventRecord {
   body: string;
@@ -14,25 +13,26 @@ export const handler = async (event: LambdaEvent) => {
   for (const record of event.Records) {
     const { data } = JSON.parse(record.body);
 
-    if (data.error) {
-      const { success, result } = await saveErrorData(data);
-      
-      if (success) {
-        sendWebhookNotification(); // fire-and-forget
-        return createResponse(200, { message: 'Error data received and logged to PostgreSQL' });
+    try {
+      if (data.error) {
+        const { success, result } = await saveErrorData(data);
+        
+        if (success) {
+          sendWebhookNotification(); // fire-and-forget
+        } else {
+          console.error('Error saving error data to PostgreSQL'); 
+        }
       } else {
-        return createResponse(500, { message: 'Error saving error data to PostgreSQL' });
-      }
-    } else {
-      const { success, result } = await savePromiseData(data);
-      console.log('PostgreSQL insert result:', result);
+        const { success, result } = await saveRejectionData(data);
 
-      if (success) {
-        sendWebhookNotification(); // fire-and-forget
-        return createResponse(200, { message: 'Promise data received and logged to PostgreSQL' });
-      } else {
-        return createResponse(500, { message: 'Error saving promise data to PostgreSQL' });
+        if (success) {
+          sendWebhookNotification(); // fire-and-forget
+        } else {
+          console.error('Error saving rejection data to PostgreSQL');
+        }
       }
+    } catch (e) {
+      console.error("Error processing record:", e);
     }
   }
 };
