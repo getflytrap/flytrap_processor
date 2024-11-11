5be059b375a9a92eac0aa621a5fefda5762f1f4a
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-11 14:38:25 +0100

    feat: added method to extract filename, line number and column number from stack trace
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- helpers/database.ts
+++ helpers/database.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv'; // only for local dev
 import pkg from 'pg';
 import { v4 as uuidv4 } from 'uuid';
+import { extractLineAndColNumbers } from './stacktrace_processor';
 
 dotenv.config(); // only for local dev
 const { Pool } = pkg;
@@ -36,16 +37,21 @@ interface RejectionData {
 export const saveErrorData = async (data: ErrorData) => {
   try {
     const project = await pool.query('SELECT id FROM projects WHERE uuid = $1', [data.project_id])
-    // TODO: Validate that project exists?
     const projectId = project.rows[0].id ? project.rows[0].id : null;
-
     if (!projectId) return { success: false, error: "Project not found."}
 
     const error_uuid = uuidv4();
+
+    console.log('Error stack trace:')
+    console.log(data.error.stack);
+    console.log('');
+    
+    const { fileName, lineNumber, colNumber} = extractLineAndColNumbers(data.error.stack);
+
     const query = `INSERT INTO error_logs (uuid, name, message, created_at,
     line_number, col_number, project_id, stack_trace, handled) VALUES 
     ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id`;
-  
+
     const result = await pool.query(
       query,
       [
@@ -53,8 +59,8 @@ export const saveErrorData = async (data: ErrorData) => {
         data.error.name || 'UnknownError',
         data.error.message || 'No message provided',
         data.timestamp,
-        data.line_number || null,
-        data.col_number || null,
+        lineNumber,
+        colNumber,
         projectId,
         data.error.stack || 'No stack trace available',
         data.handled,
@@ -72,7 +78,6 @@ export const saveErrorData = async (data: ErrorData) => {
 export const saveRejectionData = async (data: RejectionData) => {
   try {
     const project = await pool.query('SELECT id FROM projects WHERE uuid = $1', [data.project_id])
-    // TODO: Validate that project exists?
     const projectId = project.rows[0].id ? project.rows[0].id : null;
 
     if (!projectId) return { success: false, error: "Project not found."}
