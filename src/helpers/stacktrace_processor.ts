f1089430168ba78e1a274287ed5d940db6d861e3
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-15 20:39:47 +0100

    feat: conditional stack trace parsing based on project platform
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- src/helpers/stacktrace_processor.ts
+++ src/helpers/stacktrace_processor.ts
@@ -1,19 +1,40 @@
-export const extractLineAndColNumbers = (stack: string | undefined) => {
+export const extractLineAndColNumbers = (stack: string | undefined, platform: string) => {
   if (stack) {
     // const regex = /:\d+:\d+/g;
-    const regex = /(\S+?):(\d+):(\d+)/;
-    const match = stack.match(regex);
+    let regex;
+    
+    if (platform === 'Flask') {
+      regex = /File "(.+?)", line (\d+),/;
+      const match = stack.match(regex);
 
-    console.log('Match:')
-    console.log(match);
+      console.log('Python Match:')
+      console.log(match);
+      
+      if (match) {
+        const fileName = match[1];
+        const line = parseInt(match[2], 10);
+        return {
+          fileName,
+          lineNumber: line,
+          colNumber: null,
+        };
+      }
+
+    } else if (platform === 'Express.js') {
+      regex = /(\S+?):(\d+):(\d+)/;
+      const match = stack.match(regex);
   
-    if (match) {
-      const [, fileName, line, col] = match;
-      return {
-        fileName,
-        lineNumber: parseInt(line, 10),
-        colNumber: parseInt(col, 10),
-      };
+      console.log('Express Match:')
+      console.log(match);
+    
+      if (match) {
+        const [, fileName, line, col] = match;
+        return {
+          fileName,
+          lineNumber: parseInt(line, 10),
+          colNumber: parseInt(col, 10),
+        };
+      }
     }
   }
 
