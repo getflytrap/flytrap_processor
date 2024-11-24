ea7bc8f040c60ef1daef601440ea40b7550a98ad
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-24 14:00:14 +0100

    chore: cleanup and docstrings
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- src/processor.ts
+++ src/processor.ts
@@ -1,37 +1,55 @@
-import { saveErrorData, saveRejectionData } from "./helpers/database";
-import { sendWebhookNotification } from "./helpers/webhook";
-
+import { saveErrorData, saveRejectionData } from "./utils/database";
+import { sendWebhookNotification } from "./utils/webhook";
+import { CodeContext } from "./utils/types";
 interface EventRecord {
-  body: string;
+  body: string; // Represents the body of a record, typically a JSON string.
 }
 
 interface LambdaEvent {
-  Records: EventRecord[];
+  Records: EventRecord[]; // Contains an array of records received by the Lambda.
 }
 
+/**
+ * AWS Lambda handler to process event records, save error or rejection data to PostgreSQL,
+ * and send webhook notifications if successful.
+ *
+ * @param event - The event object containing an array of records.
+ */
 export const handler = async (event: LambdaEvent) => {
   for (const record of event.Records) {
     const { data } = JSON.parse(record.body);
 
+    if (data.codeContexts) {
+      data.codeContexts = data.codeContexts.map(
+        (contextEntry: CodeContext) => ({
+          ...contextEntry,
+          context: JSON.parse(contextEntry.context),
+        }),
+      );
+    }
+
     try {
       if (data.error) {
+        // Save error data to PostgreSQL and send webhook notification on success.
         const { success } = await saveErrorData(data);
-        
+
         if (success) {
           sendWebhookNotification(data.project_id); // fire-and-forget
         } else {
-          console.error('Error saving error data to PostgreSQL'); 
+          console.error("Error saving error data to PostgreSQL");
         }
       } else {
+        // Save rejection data to PostgreSQL and send webhook notification on success.
         const { success } = await saveRejectionData(data);
 
         if (success) {
           sendWebhookNotification(data.project_id); // fire-and-forget
         } else {
-          console.error('Error saving rejection data to PostgreSQL');
+          console.error("Error saving rejection data to PostgreSQL");
         }
       }
     } catch (e) {
+      // Log unexpected errors during processing.
       console.error("Error processing record:", e);
     }
   }
