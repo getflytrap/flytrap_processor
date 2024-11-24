b55dd5142c9382cb72f4c484bf1c410337aaeb11
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-24 14:01:15 +0100

    chore: cleanup
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- src/helpers/webhook.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const url = process.env.WEBHOOK_ENDPOINT;
-
-export const sendWebhookNotification = async (projectId: string) => {
-  if (url) {
-    axios.post(url, { message: 'New issue', project_id: projectId });
-  }
-}
